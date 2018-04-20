1202c07183f0407987b8016b80dc9a4ebe836900
shilei <shilei@yuewen.com>
2018-04-20 14:13:22 +0800

    init

---
+++ src/pages/menu/a.vue
@@ -0,0 +1,27 @@
+<config>
+  {
+    a: a
+  }
+</config>
+
+<template>
+  <view> {{ message }} </view>
+</template>
+
+<script>
+Page({
+  data: {
+    message: 'Hello MINA!'
+  },
+  methods: {
+    adsda: 'adassa'
+  }
+})
+</script>
+
+<style>
+
+.middle-p {
+  padding:15px;
+}
+</style>
